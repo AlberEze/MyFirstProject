f4a11cdd68a397658cf9e6d0d824a4ccbca448db
lmao <alberteze20@gmail.com>
2023-07-30 10:01:43 -0400

    My first commit.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        for(int i = 0; i < 5; i++){
+            System.out.println("Hola");
+        }
     }
 }
